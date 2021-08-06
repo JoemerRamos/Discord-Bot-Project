b58d618368ea2b669ee30fd7efb5feaa6139db7f
JoemerRamos <34356209+JoemerRamos@users.noreply.github.com>
2021-08-06 14:13:08 -0400

    UPD: Package.json to include current versions for node and npm

--- commands/animeAPI.js
+++ commands/animeAPI.js
@@ -1,25 +1,25 @@
-const { titlesRequest } = require("../mal-api/apiRequests");
-const { createCompForNextEp } = require("../mal-api/utilFunctions");
+const { titlesRequest } = require('../mal-api/apiRequests');
+const { createCompForNextEp } = require('../mal-api/utilFunctions');
 
 module.exports = {
-  name: "ask",
-  description: "Communicates with anime api",
+  name: 'ask',
+  description: 'Communicates with anime api',
   options: [
     {
-      name: "anime",
-      description: "Type name of anime",
+      name: 'anime',
+      description: 'Type name of anime',
       required: true,
-      type: 3, //Application Command Type 3 is String
+      type: 3, // Application Command Type 3 is String
     },
   ],
   async execute(interaction, options) {
-    const { value: animeName } = options.get("anime");
+    const { value: animeName } = options.get('anime');
     const animeList = await titlesRequest(animeName);
     const components = createCompForNextEp(animeList);
 
     interaction.reply({
       content: `You searched: ${animeName}\nChoose which anime title you want more details on:\n`,
-      components: components,
+      components,
     });
   },
 };
--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-require("dotenv").config();
-const { keepAlive } = require("./server");
-const fs = require("fs");
-const Discord = require("discord.js");
-const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const { createAnimeEmbedNextEp } = require("./mal-api/utilFunctions");
-const TOKEN = process.env.TOKEN;
-let cmdHandler = new Discord.Collection();
+require('dotenv').config();
+const fs = require('fs');
+const Discord = require('discord.js');
+const { keepAlive } = require('./server');
+
+const bot = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const { createAnimeEmbedNextEp } = require('./mal-api/utilFunctions');
+
+const { TOKEN } = process.env;
+const cmdHandler = new Discord.Collection();
 
 keepAlive();
 bot.login(TOKEN);
 
-bot.on("ready", async () => {
-  setCommands(cmdHandler);
-});
-
-bot.on("interactionCreate", async (interaction) => {
-  //Type 2 is a command interaction
-  if (interaction.isCommand()) {
-    const { commandName, options } = interaction;
-
-    try {
-      await cmdHandler.get(commandName).execute(interaction, options);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
-    }
-  } else if (interaction.isButton()) {
-    const { customId } = interaction;
-    if (customId.includes("nextEp")) {
-      //If we choose a specific search result, we want to output and embedded card with all the information for the search result
-      embedObj = await createAnimeEmbedNextEp(customId, Discord);
-      interaction.reply({ embeds: [embedObj] });
-    }
-  }
-});
-
-//Adds commands to a map collection so that we can dynamically access commands for interactions
+// Adds commands to a map collection so that we can dynamically access commands for interactions
 async function setCommands(cmdController) {
-  cmdObjects = [];
+  const cmdObjects = [];
 
-  const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+  const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -47,9 +24,35 @@ async function setCommands(cmdController) {
     cmdController.set(command.name, command);
   }
 
-  //Registers the slash commands to show in discord chat
-  await bot.guilds.cache.get("815610859471896596")?.commands.set(cmdObjects);
+  // Registers the slash commands to show in discord chat
+  await bot.guilds.cache.get('815610859471896596')?.commands.set(cmdObjects);
 }
+
+bot.on('ready', async () => {
+  setCommands(cmdHandler);
+});
+
+bot.on('interactionCreate', async (interaction) => {
+  // Type 2 is a command interaction
+  if (interaction.isCommand()) {
+    const { commandName, options } = interaction;
+
+    try {
+      await cmdHandler.get(commandName).execute(interaction, options);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  } else if (interaction.isButton()) {
+    const { customId } = interaction;
+    if (customId.includes('nextEp')) {
+      /* If we choose a specific search result, we want to output and
+      embedded card with all the information for the search result */
+      const embedObj = await createAnimeEmbedNextEp(customId, Discord);
+      interaction.reply({ embeds: [embedObj] });
+    }
+  }
+});
 
 async function deleteGuildCommands(guildID) {
   await bot.guilds.cache.get(guildID)?.commands.set([]);
