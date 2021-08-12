ccebb55333ca597e10eeebf4cde7dffaae87ea6e
JoemerRamos <34356209+JoemerRamos@users.noreply.github.com>
2021-08-11 20:27:06 -0400

    UPD: Added recognition between OS

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-/*Not meant as a means to handle routing between a web app, this is a web server meant
-for keeping the bot alive*/
+/* Not meant as a means to handle routing between a web app, this is a web server meant
+for keeping the bot alive */
 
-const express = require("express");
+const express = require('express');
 
 const server = express();
 
-server.all("/", (req, res) => {
-  res.send("Bot is running!");
+server.all('/', (req, res) => {
+  res.send('Bot is running!');
 });
 
 const keepAlive = () => {
   server.listen(3000, () => {
-    console.log("Server is ready.");
+    console.log('Server is ready.');
   });
 };
 
 module.exports.keepAlive = keepAlive;
-//module.exports.keepAlive = keepAlive()
+// module.exports.keepAlive = keepAlive()
